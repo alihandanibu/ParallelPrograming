0959e50f0e90d66927f42ba2b1250be207674e02
adnan-hajro <adnan.hajro@ibu.edu.ba>
2025-11-12 22:09:04 +0100

    WIP: fixing loop pragma

--- Example_05/vecadd_opt2.c
+++ Example_05/vecadd_opt2.c
@@ -19,6 +19,7 @@ int main(int argc, char *argv[]){
 
     struct timespec tstart;
     double time_sum = 0.0;
+#pragma omp parallel for
     for (int i=0; i<ARRAY_SIZE; i++) {
         a[i] = 1.0;
         b[i] = 2.0;
